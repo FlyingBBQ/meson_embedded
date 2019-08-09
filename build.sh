147dc8c7e1a7d0e15fe4c9ba9d9b957b13fedb20
Derek Cardol <derek.cardol@gmail.com>
2019-08-09 14:15:38 +0000

    updated helper scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 srcdir=.
-buildir=build
+buildir=build/debug
 
 if [ -d "$buildir" ]; then
     echo "removing directory: $buildir"
     rm -r $buildir
 fi
 
-meson $srcdir $buildir --cross-file config/cross_linux.txt
+meson $srcdir $buildir --cross-file config/cross_linux.txt --buildtype=debugoptimized
+
+cd $buildir
--- flash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/derek/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI -c port=SWD -w ./build/main.bin 0x8000000
