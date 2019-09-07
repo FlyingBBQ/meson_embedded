14fb1ceecde58a817379e56a8110cb00e8a35076
FlyingBBQ <derek.cardol@gmail.com>
2019-09-07 19:41:45 +0200

    automatic running and building docker

---
+++ env.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CONTAINER="meson_env"
+
+# check if the container exists
+#if [[ $(docker ps --filter "name=^/$name$" --format '{{.Names}}') == "$CONTAINER" ]]; then
+#    # run the container
+#    docker run -it -v $(pwd):/project -h build $CONTAINER bash
+#else 
+#    # build the container
+#    docker build config/ -t $CONTAINER
+#    # run the container
+#    docker run -it -v $(pwd):/project -h build $CONTAINER bash
+#fi
+
+#docker image inspect $CONTAINER:latest >/dev/null 2>&1 && docker run -it -v $(pwd):/project -h build $CONTAINER bash || docker build config/ -t $CONTAINER
+
+docker build config/ -t $CONTAINER --build-arg user=$USER --build-arg userid=$UID
+docker run -it -v $(pwd):/project -h build -e USER=$USER -e USERID=$UID $CONTAINER bash 
